3dfc09e9b30531a2193a9acb24757ab36f806b93
Joe Sherwood <na>
2019-11-15 11:33:18 -0600

    Add form with 11 items in 2 sections

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -4,13 +4,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  var body: some View {
+    Form {
+      Section {
         Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+      }
+      Section {
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
