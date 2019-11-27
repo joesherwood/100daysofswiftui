c841be1ba9bacbc12dd713bec5b41b218e44818c
Joe Sherwood <na>
2019-11-26 22:53:43 -0600

    Create guesstheflag project

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,16 @@
+// GuessTheFlag (11/26/19)
+// © 2019 Joe Sherwood. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
