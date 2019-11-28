0078998848283b17cf4b7e36187059937a5aafbc
Joe Sherwood <na>
2019-11-27 21:29:31 -0600

    Display 3 flags to choose from

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -4,8 +4,33 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+  var correctAnswer = Int.random(in: 0...2)
+  
   var body: some View {
-    Text("stuff")
+    ZStack{
+      Color.blue.edgesIgnoringSafeArea(.all)
+      
+      VStack(spacing: 30) {
+        VStack {
+          Text("Tap the flag of")
+            .foregroundColor(Color.white)
+          Text(countries[correctAnswer])
+            .foregroundColor(Color.white)
+        }
+        
+        ForEach(0 ..< 3) { number in
+          Button(action: {
+          }) {
+            Image(self.countries[number])
+              .renderingMode(.original)
+          }
+        }
+        
+        Spacer()
+      }
+    }
   }
 }
 
