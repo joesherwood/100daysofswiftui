ac09a14563fba00fcdd13ebc8d90635dc02a45d5
Joe Sherwood <na>
2019-11-27 21:50:38 -0600

    Clip each flag to capsule shape

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
   
   var body: some View {
     ZStack{
-      Color.blue.edgesIgnoringSafeArea(.all)
+      LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
       
       VStack(spacing: 30) {
         VStack {
@@ -21,6 +21,8 @@ struct ContentView: View {
             .foregroundColor(Color.white)
           Text(countries[correctAnswer])
             .foregroundColor(Color.white)
+            .font(.largeTitle)
+            .fontWeight(.black)
         }
         
         ForEach(0 ..< 3) { number in
@@ -29,6 +31,9 @@ struct ContentView: View {
           }) {
             Image(self.countries[number])
               .renderingMode(.original)
+              .clipShape(Capsule())
+              .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+              .shadow(color: .black, radius: 2)
           }
         }
         
