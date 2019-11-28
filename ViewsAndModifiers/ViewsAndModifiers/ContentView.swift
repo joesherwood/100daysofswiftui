3283e0fb5a0383cf85ddc2efecd6e14631f39d48
Joe Sherwood <na>
2019-11-27 23:23:54 -0600

    Encapsulate button properties as a capsuletext view

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -4,15 +4,12 @@
 import SwiftUI
 
 struct ContentView: View {
-  var motto1: some View { Text("Draco dormiens") }
-  let motto2 = Text("nanquam titillandus")
-  
   var body: some View {
-    VStack {
-      motto1
-        .foregroundColor(.red)
-      motto2
-        .foregroundColor(.blue)
+    VStack(spacing: 10) {
+      CapsuleText(text: "1th")
+        .foregroundColor(.white)
+      CapsuleText(text: "2th")
+        .foregroundColor(.yellow)
     }
   }
 }
@@ -20,5 +17,17 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+
+
+struct CapsuleText: View {
+  var text: String
+  var body: some View {
+    Text(text)
+      .font(.largeTitle)
+      .padding()
+      .background(Color.blue)
+      .clipShape(Capsule())
   }
 }
