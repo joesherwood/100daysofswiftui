4da4a350ea10c3899bd23fba62fefd38ca873997
Joe Sherwood <na>
2019-11-27 19:09:01 -0600

    Fill fullscreen red background in vstack

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -5,23 +5,12 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      HStack {
-        Text("1")
-        Text("2")
-        Text("3")
-      }
-      HStack {
-        Text("4")
-        Text("5")
-        Text("6")
-      }
-      HStack {
-        Text("7")
-        Text("8")
-        Text("9")
-      }
+
+    ZStack {
+      Color.red.edgesIgnoringSafeArea(.all)
+      Text("Your content")
     }
+    
   }
 }
 
