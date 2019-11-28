078edba08fb68bb6feea726a9779dc1262a8eb16
Joe Sherwood <na>
2019-11-27 21:57:25 -0600

    Keep track of correct answers [c1]

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
   
   @State private var showingScore = false
   @State private var scoreTitle = ""
+  @State private var correctTotal = 0
   
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
   @State private var correctAnswer = Int.random(in: 0...2)
@@ -41,7 +42,7 @@ struct ContentView: View {
       }
     }
     .alert(isPresented: $showingScore) {
-      Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+      Alert(title: Text(scoreTitle), message: Text("Your score is \(correctTotal)"), dismissButton: .default(Text("Continue")) {
         self.askQuestion()
       })
     }
@@ -50,6 +51,7 @@ struct ContentView: View {
   func flagTapped(_ number: Int) {
     if (number == correctAnswer) {
       scoreTitle = "Correct"
+      correctTotal += 1
     }
     else {
       scoreTitle = "Wrong"
