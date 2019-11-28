16dc9e50af2342542ddc8d7f715bc28cfaa281a7
Joe Sherwood <na>
2019-11-27 21:42:43 -0600

    Determine correctness of user flag selection

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -5,8 +5,11 @@ import SwiftUI
 
 struct ContentView: View {
   
-  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-  var correctAnswer = Int.random(in: 0...2)
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  @State private var correctAnswer = Int.random(in: 0...2)
   
   var body: some View {
     ZStack{
@@ -22,6 +25,7 @@ struct ContentView: View {
         
         ForEach(0 ..< 3) { number in
           Button(action: {
+            self.flagTapped(number)
           }) {
             Image(self.countries[number])
               .renderingMode(.original)
@@ -31,7 +35,29 @@ struct ContentView: View {
         Spacer()
       }
     }
+    .alert(isPresented: $showingScore) {
+      Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+        self.askQuestion()
+      })
+    }
   }
+  
+  func flagTapped(_ number: Int) {
+    if (number == correctAnswer) {
+      scoreTitle = "Correct"
+    }
+    else {
+      scoreTitle = "Wrong"
+    }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
