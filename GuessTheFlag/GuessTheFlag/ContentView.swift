4356c0670c240e8acdf0000b75e6dfa52433a64c
Joe Sherwood <na>
2019-11-27 18:49:44 -0600

    Build 3x3 grid with hstack and vstack

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -4,13 +4,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  var body: some View {
+    VStack {
+      HStack {
+        Text("1")
+        Text("2")
+        Text("3")
+      }
+      HStack {
+        Text("4")
+        Text("5")
+        Text("6")
+      }
+      HStack {
+        Text("7")
+        Text("8")
+        Text("9")
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
