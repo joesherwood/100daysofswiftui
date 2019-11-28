5d166115d1fa28ce4fc18c6ceaae97dfadc613f1
Joe Sherwood <na>
2019-11-27 23:57:11 -0600

    Define a custom viewmodifier for titles [c1]

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -5,10 +5,8 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    GridStack(rows: 4, columns: 4) { row, col in
-      Image(systemName: "\(row * 4 + col).circle")
-      Text("r\(row),c\(col)")
-    }
+    Text("Hello World")
+      .titleStyle()
   }
 }
 
@@ -18,27 +16,16 @@ struct ContentView_Previews: PreviewProvider {
   }
 }
 
-
-struct GridStack<Content: View>: View {
-  let rows: Int
-  let columns: Int
-  let content: (Int, Int) -> Content
-  
-  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-    self.rows = rows
-    self.columns = columns
-    self.content = content
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundColor(.blue)
   }
-  
-  var body: some View {
-    VStack {
-      ForEach(0 ..< rows) { row in
-        HStack {
-          ForEach(0 ..< self.columns) { column in
-            self.content(row, column)
-          }
-        }
-      }
-    }
+}
+
+extension View {
+  func titleStyle() -> some View {
+    self.modifier(Title())
   }
 }
