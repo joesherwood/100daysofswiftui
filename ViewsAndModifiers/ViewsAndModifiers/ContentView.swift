383ed73360b003fa4f24f4edd08e4ce151658c3d
Joe Sherwood <na>
2019-11-27 22:35:56 -0600

    Create new project viewsandmodifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,16 @@
+// ViewsAndModifiers (11/27/19)
+// © 2019 Joe Sherwood. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
