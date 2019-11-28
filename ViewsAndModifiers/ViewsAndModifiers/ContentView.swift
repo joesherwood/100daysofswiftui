acca56a28f234130bc75bce779a8c44e9e75bd9f
Joe Sherwood <na>
2019-11-27 22:54:23 -0600

    Extend the red text background to infinity

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -4,13 +4,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    Text("Hello, World!")
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color.red)
+      .edgesIgnoringSafeArea(.all)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
