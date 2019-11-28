97a30c505c88f6fa3934672397b0e0effaf2f481
Joe Sherwood <na>
2019-11-27 23:47:09 -0600

    Use a custom container (gridstack) to show a 4x4 display

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -5,9 +5,10 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Color.blue
-    .frame(width: 300, height: 300)
-    .watermarked(with: "Hacking With Swift")
+    GridStack(rows: 4, columns: 4) { row, col in
+      Image(systemName: "\(row * 4 + col).circle")
+      Text("r\(row),c\(col)")
+    }
   }
 }
 
@@ -17,39 +18,27 @@ struct ContentView_Previews: PreviewProvider {
   }
 }
 
-struct Title: ViewModifier {
-  func body(content: Content) -> some View {
-    content
-      .font(.largeTitle)
-      .foregroundColor(.white)
-      .padding()
-      .background(Color.blue)
-      .clipShape(RoundedRectangle(cornerRadius: 10))
-  }
-}
 
-struct Watermark: ViewModifier {
-  var text: String
+struct GridStack<Content: View>: View {
+  let rows: Int
+  let columns: Int
+  let content: (Int, Int) -> Content
   
-  func body(content: Content) -> some View {
-    ZStack(alignment: .bottomTrailing) {
-      content
-      Text(text)
-        .font(.caption)
-        .foregroundColor(.white)
-        .padding(5)
-        .background(Color.black)
+  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+    self.rows = rows
+    self.columns = columns
+    self.content = content
+  }
+  
+  var body: some View {
+    VStack {
+      ForEach(0 ..< rows) { row in
+        HStack {
+          ForEach(0 ..< self.columns) { column in
+            self.content(row, column)
+          }
+        }
+      }
     }
-  }
-}
-
-
-extension View {
-  func titleStyle() -> some View {
-    self.modifier(Title())
-  }
-  
-  func watermarked(with text: String) -> some View {
-    self.modifier(Watermark(text: text))
   }
 }
