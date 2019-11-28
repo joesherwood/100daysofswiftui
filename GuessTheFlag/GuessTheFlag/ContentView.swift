fe744737be2dccf3334e4244d2cbf622159a226a
Joe Sherwood <na>
2019-11-27 22:03:25 -0600

    Present correct response in alert after a wrong selection [c3]

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -59,7 +59,7 @@ struct ContentView: View {
       correctTotal += 1
     }
     else {
-      scoreTitle = "Wrong"
+      scoreTitle = "Wrong, that's the flag of \(countries[number])"
     }
     
     showingScore = true
