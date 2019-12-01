5ca7a8b29016fc8573be9066d3f2f3cd8c35fd23
Joe Sherwood <na>
2019-11-30 23:42:23 -0600

    Display cpu's move

--- TrainRPS/TrainRPS/ContentView.swift
+++ TrainRPS/TrainRPS/ContentView.swift
@@ -3,9 +3,25 @@
 
 import SwiftUI
 
+enum Move: String, CaseIterable {
+  case rock = "hexagon.fill"
+  case paper = "map.fill"
+  case scissors = "scissors"
+}
+
 struct ContentView: View {
+  @State var cpuMove = Move.allCases.randomElement() ?? Move.rock
+  @State var winningMove = Bool.random()
+  
   var body: some View {
-    Text("Hello, World!")
+    HStack {
+      Text(winningMove ? "Beat" : "Lose to")
+        .font(.title)
+      Image(systemName: cpuMove.rawValue)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 50, height: 50)
+    }
   }
 }
 
