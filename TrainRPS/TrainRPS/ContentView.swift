6580d675c9a61018a3f23dfc0e7e2b5e482f1b6f
Joe Sherwood <na>
2019-12-01 17:46:24 -0600

    Add green background

--- TrainRPS/TrainRPS/ContentView.swift
+++ TrainRPS/TrainRPS/ContentView.swift
@@ -39,38 +39,47 @@ struct ContentView: View {
   @State var final: Bool = false
   
   var body: some View {
-    VStack {
-      Text("Score \(score.line())")
-        .font(.body)
-        .fontWeight(.thin)
-      
-      ZStack {
-        RoundedRectangle(cornerRadius: 10)
-          .foregroundColor(.gray)
-        HStack {
-          Text(shouldWin ? "Beat" : "Lose to")
-            .font(.title)
-          Image(systemName: cpuMove.rawValue)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 50, height: 50)
-        }
-      }
-      .frame(maxWidth: 300, maxHeight: 100)
+    ZStack {
+      Color.green
+        .edgesIgnoringSafeArea(.all)
       
       VStack {
-        ForEach(Move.allCases, id: \.self) { move in
-          Button(action: {
-            self.checkAnswer(user: move, cpu: self.cpuMove, shouldWin: self.shouldWin)
-          }) {
-            ActionImage(name: move.rawValue)
+        Text("Score \(score.line())")
+          .font(.title)
+          .fontWeight(.thin)
+          .foregroundColor(.white)
+        
+        ZStack {
+          RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(Color(red: 0, green: 0.5, blue: 0))
+          HStack {
+            Text(shouldWin ? "Beat" : "Lose to")
+              .font(.title)
+              .fontWeight(.semibold)
+              .foregroundColor(.white)
+            Image(systemName: cpuMove.rawValue)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .foregroundColor(.white)
           }
-          .frame(height: 100)
         }
-        .padding([.top, .bottom], 10)
+        .frame(maxWidth: 300, maxHeight: 100)
+        
+        VStack {
+          ForEach(Move.allCases, id: \.self) { move in
+            Button(action: {
+              self.checkAnswer(user: move, cpu: self.cpuMove, shouldWin: self.shouldWin)
+            }) {
+              ActionImage(name: move.rawValue)
+            }
+            .frame(height: 100)
+          }
+          .padding([.top, .bottom], 10)
+        }
+        .padding([.top], 40)
+        
       }
-      .padding([.top], 40)
-      
     }
     .alert(isPresented: $final) {
       Alert(title: Text("Final"), message: Text("Your score is \(score.line())"), dismissButton: .default(Text("New Game")) {
