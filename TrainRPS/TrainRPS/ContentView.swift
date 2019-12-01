3f7d184c2334458e57dfd4b016b720d7544df3b7
Joe Sherwood <na>
2019-12-01 00:03:34 -0600

    Show current line score

--- TrainRPS/TrainRPS/ContentView.swift
+++ TrainRPS/TrainRPS/ContentView.swift
@@ -9,24 +9,54 @@ enum Move: String, CaseIterable {
   case scissors = "scissors"
 }
 
+struct Score {
+  var correct: Int
+  var attempts: Int
+  
+  init() {
+    self.correct = 0
+    self.attempts = 0
+  }
+  
+  mutating func win() {
+    self.correct += 1
+    self.attempts += 1
+  }
+  
+  mutating func lose() {
+    self.attempts += 1
+  }
+  
+  func line() -> String {
+    return "\(self.correct) for \(self.attempts)"
+  }
+}
+
 struct ContentView: View {
   @State var cpuMove = Move.allCases.randomElement() ?? Move.rock
-  @State var winningMove = Bool.random()
+  @State var shouldWin = Bool.random()
+  @State var score: Score = Score()
   
   var body: some View {
-    HStack {
-      Text(winningMove ? "Beat" : "Lose to")
-        .font(.title)
-      Image(systemName: cpuMove.rawValue)
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-        .frame(width: 50, height: 50)
+    VStack {
+      Text("Score \(score.line())")
+        .font(.body)
+        .fontWeight(.thin)
+      
+      HStack {
+        Text(shouldWin ? "Beat" : "Lose to")
+          .font(.title)
+        Image(systemName: cpuMove.rawValue)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 50, height: 50)
+      }
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(score: Score())
   }
 }
