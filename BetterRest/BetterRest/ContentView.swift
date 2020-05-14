187b6695bdb25ebebec3d73254909265bb0b8bc2
Joe Sherwood <na>
2020-05-14 18:48:31 -0400

    Create project for BetterRest

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,16 @@
+// BetterRest (5/14/20)
+// © 2020 Joe Sherwood. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
