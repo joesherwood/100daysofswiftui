f9dc5e0bb8b3a0d32a03398e30330f66df0c492a
Joe Sherwood <na>
2020-05-14 22:56:37 -0400

    Use model to determine sleep time

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,6 +9,10 @@ struct ContentView: View {
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
   
+  @State private var alertTitle = ""
+  @State private var alertMessage = ""
+  @State private var showingAlert = false
+  
   var body: some View {
     NavigationView {
       VStack {
@@ -43,11 +47,34 @@ struct ContentView: View {
           Text("Calculate")
         }
       )
+        .alert(isPresented: $showingAlert) {
+          Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+      }
     }
   }
   
   func calculateBedtime() {
+    let model = BetterRestSleep()
+    let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+    let seconds = hour * 60 * 60 + minute * 60
     
+    do {
+      let prediction = try model.prediction(wake: Double(seconds), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+      let sleepTime = wakeUp - prediction.actualSleep
+      
+      let formatter = DateFormatter()
+      formatter.timeStyle = .short
+      
+      alertTitle = "Your ideal sleep time is"
+      alertMessage = formatter.string(from: sleepTime)
+    }
+    catch {
+      alertTitle = "Error"
+      alertMessage = "Problem calcuating sleep time"
+    }
+    showingAlert = true
   }
 }
 
